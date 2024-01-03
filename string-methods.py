46b6fd17e08fb2bd78f6e28e5e97048543b6c412
lukasthecodinggenius <92871428+lukasthecodinggenius@users.noreply.github.com>
2024-01-03 02:59:51 -0500

    Create string-methods.py

---
+++ string-methods.py
@@ -0,0 +1,14 @@
+course = 'Python for Beginners'
+
+course.upper()
+# changes to uppercase
+course.lower()
+# changes to lowercase
+course.title()
+# capitalizes the first letter in every word
+course.find()
+# finds where certain characters / sequences are in the code
+course.replace()
+# replaces values with other values
+print('...' in course)
+# checks if certain values are contained in the string
